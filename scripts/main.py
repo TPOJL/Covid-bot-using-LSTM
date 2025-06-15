a00dde57231520a6cdcf8eb667225c5435ac00cb
TPOJL <mvtonkiy@gmail.com>
2025-06-15 17:44:53 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,6 @@
 from application import Application
 from model_manager import ModelManager
 
-#start the program
 if __name__ == '__main__':
     model = ModelManager()
     app = Application(model)
